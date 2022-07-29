c5e5c3273d43c56040c793aa1280ccaad56d559e
israelBlackyb <91918435+israelBlackyb@users.noreply.github.com>
2022-07-29 12:29:07 +1200

    Upload more

--- Scenario1_IsraelBlack/Scenario1_IsraelBlack.cpp
+++ Scenario1_IsraelBlack/Scenario1_IsraelBlack.cpp
@@ -1,53 +1,95 @@
  #include <iostream>
+#include <vector>
 
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
+using std::vector;
 
 
 class Location {
-    int degrees;
-    float minutes;
-    char direction;
+    int degreesLong, degreesLat;
+    float minutesLong, minutesLat;
+    char directionLong, directionLat;
 
 public:
 
     void getpos() {
-        cout << "\n\nInput degrees between 0 and 180: "; //Latitude
-        cout << "\nInput mintues between 0 and 60: ";
-        cout << "\nInput direction(N/S/E/W): ";
+    //latitude
+        cout << "Enter latitude measurements: "; 
+        cout << "\nInput Direction (N/S): ";  cin >> directionLat;
+        cout << "\nInput Degrees between 0 and 90: "; cin >> degreesLat;
+        cout << "\nInput Minutes: "; cin >> minutesLat;
+        cout << "\n**********************\n\n";
+    //longitude
+        cout << "Enter Longitude measurements: \n";
+        cout << "Input Direction (E/W): \n"; cin>>directionLong;
+        cout << "Input Degrees between 0 and 180: \n"; cin >> degreesLong;
+        cout << "Input Minutes: \n"; cin >> minutesLong;
+        cout << "**********************\n**********************\n\n";
 
-        cout << "\n\nInput degrees between 0 and 180: "; //Longitude
-        cout << "\nInput mintues between 0 and 60: ";
-        cout << "\nInput direction(N/S/E/W): ";
+        //checking user inputs
+
+        
+
+        if (directionLat != 'N' || directionLat != 'S' || directionLat != 'n' || directionLat != 's') {
+            cout << "Incorrect input, please enter correct Direction";
+            directionLat = 0;
+            getpos();
+        }
+
+        if (degreesLat < 90 || degreesLat >0) {
+            cout << "Incorrect input, please enter Degrees within range\n";
+            getpos();
+        }
+
+        if (minutesLat < 60 || degreesLat> 0) {
+            cout << "Incorrect input, please enter Minutes greater than 0\n";
+            getpos();
+        }
     }
 
-    Location(int dgrs= 0, float mins= 0, char drct= 'a') {
-        degrees = dgrs;
-        minutes = mins;
-        direction = drct;
+    Location() {
+        
     }
-
 };
 
-class Yacht : public Location {
-    int yachtNumber;
+class Yacht {
     Location location;
     
 public:
+    int yachtNumber;
     void get_pos(){
         location.getpos();
     }
 
-    Yacht  (int yachtNum, Location loc) {
+    Yacht  (int yachtNum) {
         yachtNumber = yachtNum;
-        location = loc;
     }
+
+    Yacht() {}
 };
 
 int main()
 {
+    Yacht newYacht;
+    cout << "*************Oceans Race 2021-2022***********" << endl;
+    cout << "\n***********************" << endl;
     
+    for (int i = 0; i < 3; i++) {
+        newYacht.get_pos();
+    }
+
+    Yacht;
+    vector <Yacht*> Yachts;
+
+    for (int i = 0; i < 3; i++) {
+        Yachts.push_back(new Yacht(i+1));
+    }
+    
+    for (Yacht* yacht : Yachts) {
+        cout << yacht->yachtNumber;
+    }
 }
 
