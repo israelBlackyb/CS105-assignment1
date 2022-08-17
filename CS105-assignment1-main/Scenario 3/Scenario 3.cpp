3c59ff58f32e7896d352d05c11b14123c2125353
Israel <270161698@yoobeestudent.ac.nz>
2022-08-18 11:49:55 +1200

    Cont Scnario 3

--- CS105-assignment1-main/Scenario 3/Scenario 3.cpp
+++ CS105-assignment1-main/Scenario 3/Scenario 3.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
+using std::endl;
+using std::string;
 
 void mainMenu();
 void drawLines(int length);
+void drawSquare();
 void drawRectangle();
 void drawTriangle();
 void drawCircle();
 
 class Shapes { // Parent Class - basic measurements that all shapes contain
-
-    
-public:
+private:
     double base;
     double height;
     double result;
-    
 
+public:
     double getData() {
         return base;
         return height;
@@ -25,28 +27,68 @@ public:
     }
 
     double calculateArea() {
-        
+        double area = base*base;
+        return area;
     }
 
-    void giveResult() {
-        
+    double calculatePerimeter() {
+
+    }
+
+    void giveResult(int area) {
+        cout << "The result is: " << area << " square centimeters (sq.cm)";
+    }
+
+    void shapeMenu(string var1, string var2) {
+        int shapeMenuInput;
+        cout << "\n1. Area (area = " << var1 << " * " << var2 << " sq.units)\n";
+        cout << "\n2. Perimeter ()\n";
+        cout << "\n3. Go back to main menu\n";
+
+        cin >> shapeMenuInput;
+    }
+
+};
+
+class Squares : public Shapes { // Child Class
+    
+public:
+    
+
+    double calculateArea(double base) {
+        double area = base * base;
+        return area;
+        giveResult(area);
+   }
+
+    double calculatePerimeter(double height) {
+        double perimeter = height * 4;
+        return perimeter;
+        giveResult(perimeter);
     }
 };
 
-class Square { // Child Class
+class Rectangle : public Shapes { // Child Class
 public:
 
+    double calculateArea(double base) {
+        double area = base * base;
+        return area;
+        giveResult(area);
+    }
+
+    double calculatePerimeter(double height) {
+        double perimeter = height * 4;
+        return perimeter;
+        giveResult(perimeter);
+    }
 };
 
-class Rectangle { // Child Class
+class Triangle : public Shapes { // Child Class
 
 };
 
-class Triangle { // Child Class
-
-};
-
-class Circle { // Child Class
+class Circle : public Shapes { // Child Class
 
 };
 
@@ -55,19 +97,28 @@ int main()
     drawLines(20); //create title {
     cout << "\nShapes Calculator\n\n"; 
     drawLines(20);// }
+    cout << "\n\n";
+    
 
     mainMenu();
 }
 
 void mainMenu() {
+    int area;
+    int perimeter;
     int inputOption = 0;
+    Squares square;
+
+
 
     cout << "\n1. Square \n2. Rectangle \n3. Triangle \n4. Circle \n5. Exit\n\n"; cin >> inputOption;
 
     switch (inputOption)
     {
     case 1:
-
+        drawSquare();
+        
+        
         break;
 
     case 2:
@@ -97,15 +148,14 @@ void drawLines(int length) {
      }
 }
 
-void drawRectangle() {
-
+void drawSquare() {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+          drawLines(1); cout << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n\n";
 }
 
-void drawTriangle() {
-
-}
-
-void drawCircle() {
-
-}
 
