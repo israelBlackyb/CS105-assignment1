46cd5ce4cc1d2325593e7897eddf2cf6030d3404
Israel Black <270161698@yoobeestudent.ac.nz>
2022-08-21 08:44:43 +1200

    Finished rectangle options

--- Scenario 3/Scenario 3.cpp
+++ Scenario 3/Scenario 3.cpp
@@ -8,44 +8,74 @@ using std::string;
 
 void mainMenu();
 void menuSquare();
+void menuRectangle();
+void menuTriangle();
+
 void drawLines(int length);
 void drawSquare();
 void drawRectangle();
 void drawTriangle();
 void drawCircle();
 
+enum shape {
+    sqre,
+    rect
+};
+
+shape s = sqre;
+
 class Shapes { // Parent Class - basic measurements that all shapes contain
 private:
     double base=0;
     double height=0;
-    double radius=0;
-    double result=0;
-    double calcResult=0;
+    double radius = 0;
+    
+
+protected:
     double input;
 
 protected:
     const double pi = 3.141;
+    double calcResult = 0;
+    double result = 0;
 
 public:
 
-    void setData(int i) { //I think the Issue is here
+    void setData(int i) {
         input = i;
-        cout << "\nInput is: " << input; //outputs memory address (i think) here
     }
 
     double getData() {
-        
+        cin >> input;
         return input;
     }
 
     double calculateArea(double height, double base) {   //calculates for square and rectangle
-        setData(input);
-        height = getData();
-        cout << height;
-        result = (height + base)*2;
-        cout << endl << result;
+      
+
+        switch (s) {
+        case rect:
+            cout << "Enter Width of Rectangle: ";
+            base = getData();
+            setData(input);
+            
+        case sqre: 
+            cout << "Enter Height of Shape: ";
+            height = getData();
+            setData(input);
+            break;
+        }
+        
+        if (base < 1) {           //Measures sqaures
+            result = height * height;
+        }
+
+        else {
+            result = (height + base) * 2; //measures for rectangles
+        }
+   
         calcResult = result;
-        cout << endl << result;
+  
         giveResult();
         return result;
     }
@@ -75,24 +105,29 @@ public:
 class Squares : public Shapes { // Child Class
     
 public:
-    double calculatePerimeter(double height, double base) {
-        cout << "\nEnter Height in Centimeters: "; height = getData();
-        double perimeter = height * 4;
-        setData(perimeter);
-        return perimeter;
+    double calculatePerimeter(double height) {
+        height = getData();
+        setData(input);
+        result = height * 4;
+        calcResult = result;
         giveResult();
+        return calcResult;
     }
 };
 
 class Rectangle : public Shapes { // Child Class
 public:
     double calculatePerimeter(double height, double base) {
-        cout << "\nEnter Height in Centimeters: ";  height = getData();
-        cout << "\nEnter Base in Centimeters: "; base = getData();
-        double perimeter = (height * base)*2;
-        setData(perimeter);
-        return perimeter;
+        cout << "\nEnter Height of Rectangle: ";
+        height = getData();
+        setData(input);
+
+        cout << "\nEnter Width of Rectangle: ";
+        base = getData();
+        result = (height + base)*2;
+        calcResult = result;
         giveResult();
+        return calcResult;
     }
 };
 
@@ -128,16 +163,12 @@ int main()
     drawLines(20);// }
     cout << "\n\n";
     
-    
     mainMenu();
 }
 
 void mainMenu() {
     int inputOption = 0;
-    Squares square;
-
-
-
+    cout << "\n"; drawLines(20); cout << "\nMain Menu\n\n"; drawLines(20);
     cout << "\n1. Square \n2. Rectangle \n3. Triangle \n4. Circle \n5. Exit\n\n"; cin >> inputOption;
 
     switch (inputOption)
@@ -147,7 +178,7 @@ void mainMenu() {
         break;
 
     case 2:
-        drawRectangle();
+        menuRectangle();
         break;
 
     case 3:
@@ -178,28 +209,79 @@ void menuSquare() {
     cout << "\n\n"; drawSquare();
 
     cout << "Select from following options: \n";
-    cout << "1. Area (Area = base * base sq.units)\n";
-    cout << "2. Perimeter (Perimeter = 4 * base sq.units)\n";
+    cout << "1. Area (Area = height * height sq.units)\n";
+    cout << "2. Perimeter (Perimeter = 4 * height sq.units)\n";
     cout << "3. Go back to Main Menu (Shapes Calculator)\n\n";
     cout << "Please choose your option between 1 and 3: "; cin >> inputOption;
 
     switch (inputOption)
     {
     case 1:
-        
-        cout << "\n\nPlease Enter One Side of the Square in centimeteres: "; cin >> input;
-        square.calculateArea(height, base); 
+
+        cout << "\n\nPlease Enter One Side of the Square in centimeteres: ";
+        square.calculateArea(height);
+        drawLines(45);
+        drawLines(45);
+        cout << "\n\n\n";
+        menuSquare();
         break;
     case 2:
-
+        cout << "\n\nPlease Enter One Side of the Square in centimeteres: ";
+        square.calculatePerimeter(height);
         break;
 
     case 3:
-
+        mainMenu();
         break;
     default:
+        cout << "\nInvalid Option. Please select from options 1-3";
+        menuSquare();
         break;
     }
+}
+
+void menuRectangle() {
+    int inputOption = 0;
+    double height = 0; 
+    double base = 0; 
+    Rectangle rectangle;
+    double input;
+
+    s = rect;
+
+    drawLines(20); cout << "\nRectangle Calculator\n"; drawLines(20);
+    cout << "\n\n"; drawRectangle();
+
+    cout << "Select from following options: \n";
+    cout << "1. Area (Area = Height x Width sq.units)\n";
+    cout << "2. Perimeter (Perimeter = (Height + Width) x 2 sq.units)\n";
+    cout << "3. Go back to Main Menu (Shapes Calculator)\n\n";
+    cout << "Please choose your option between 1 and 3: "; cin >> inputOption;
+
+    switch (inputOption)
+    {
+    case 1:
+        rectangle.calculateArea(height, base);
+        drawLines(45);
+        drawLines(45);
+        cout << "\n\n\n";
+        menuRectangle();
+        break;
+    case 2:
+        rectangle.calculatePerimeter(height, base);
+        break;
+
+    case 3:
+        mainMenu();
+        break;
+    default:
+        cout << "\nInvalid Option.\n\n";
+        menuSquare();
+        break;
+    }
+}
+
+void menuTriangle() {
 
 }
 
